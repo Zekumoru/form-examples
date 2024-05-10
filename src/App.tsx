dea871c3c5bf5cec7bb07274ca6cac536bb80ff2
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-05-10 18:29:16 +0200

    Add favicon

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,34 @@ function App() {
   }, []);
 
   return (
-    <main className="p-4 max-w-screen-xl mx-auto">
-      <Form />
-    </main>
+    <>
+      <main className="p-4 max-w-screen-xl mx-auto">
+        <Form />
+      </main>
+      <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+        <aside>
+          <p>
+            Copyright ©{' '}
+            <a
+              href="https://github.com/Zekumoru/form-examples"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Zekumoru
+            </a>{' '}
+            |{' '}
+            <a
+              href="https://www.flaticon.com/free-icons/google-forms"
+              title="google forms icons"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Google forms icons created by Freepik - Flaticon
+            </a>
+          </p>
+        </aside>
+      </footer>
+    </>
   );
 }
 
