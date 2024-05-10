36856012e4636682df6621a34dc57ee897317337
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-05-10 16:22:31 +0200

    Add regex option to text input

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,25 +1,42 @@
+import { useEffect, useRef } from 'react';
+
 const TextInput = ({
   label,
   value,
   setValue,
   errorMessage,
   required,
+  regex,
 }: {
   label: string;
   value: string;
   setValue: (value: string) => void;
   errorMessage?: string;
   required?: boolean;
+  regex?: RegExp;
 }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (value === '') return;
+
+    if (regex && !regex.test(value)) {
+      inputRef.current?.setCustomValidity(
+        'Please respect the format of this field.'
+      );
+    } else {
+      inputRef.current?.setCustomValidity('');
+    }
+  }, [value, regex]);
+
   return (
     <div className="form-control">
       <label htmlFor="text-field" className="label">
         <span className="label-text">{label}</span>
       </label>
       <input
+        ref={inputRef}
         type="text"
-        name="text-field"
-        id="text-field"
         className={`input input-bordered ${errorMessage ? 'input-error' : ''}`}
         placeholder="Type here"
         value={value}
